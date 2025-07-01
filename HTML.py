1407a6a83c8b816d4ba0ce7e16397b54392c22d5
Nika Kalinnikova <NicaWilliams@yandex.ru>
2025-07-02 01:05:09 +0300

    Add files via upload

---
+++ HTML.py
@@ -0,0 +1,6 @@
+<form action="https://hayleyrizhik.app.n8n.cloud/webhook/enhance-text" method="POST">
+  <label>Ваш текст:<br>
+    <textarea name="text" rows="6" cols="40"></textarea>
+  </label><br>
+  <button type="submit">Отправить</button>
+</form>
